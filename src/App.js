915fc2be66f86cbe7067d0ee85989d0d1317450d
Kriss <krissanc@live.se>
2023-02-26 20:20:11 +0100

    overhaul page design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './NewYork.png';
+import Navbar from './Navbar.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="City">
+
+      <Navbar className="Navbar" />
+
+      <div> <img src={image} alt="" /> </div>
+
     </div>
   );
 }
